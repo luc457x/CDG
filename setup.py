504e78f3dfb7e95b807ecadf325fdc5f346d1b5b
luc45 <lucas7eixeir4@outlook.com>
2022-08-02 20:43:10 -0300

    Updated setup files.

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='CriptoDataGathering',
+    name='cdg',
     version='0.0.1',
-    packages=find_packages()
+    install_requires=[
+        'matplotlib==3.5.2',
+        'numpy==1.22.4',
+        'pandas==1.4.2',
+        'pandas_datareader==0.10.0',
+        'pycoingecko==2.2.0',
+        'python_dateutil==2.8.2',
+        'requests_cache==0.9.5',
+        'seaborn==0.11.2'
+    ]
 )
