a25b3d7539b50b108ecc65811ff8eac36b1dd50d
luc45 <lucas7eixeir4@outlook.com>
2022-08-02 21:58:42 -0300

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,15 @@
+setup(
+    name='cdg',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib==3.5.2',
+        'numpy==1.22.4',
+        'pandas==1.4.2',
+        'pandas_datareader==0.10.0',
+        'pycoingecko==2.2.0',
+        'python_dateutil==2.8.2',
+        'requests_cache==0.9.5',
+        'seaborn==0.11.2',
+    ]
+)
